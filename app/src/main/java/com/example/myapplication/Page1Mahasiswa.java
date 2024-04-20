6bf3d69fbc7a6637a04425377ebacf7200d4d7d4
SuryaHidayat93 <114637394+SuryaHidayat93@users.noreply.github.com>
2024-04-20 22:31:57 +0700

    update4

--- app/src/main/java/com/example/myapplication/Page1Mahasiswa.java
+++ app/src/main/java/com/example/myapplication/Page1Mahasiswa.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,13 +12,14 @@ import android.view.View;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Page2Dosen extends AppCompatActivity {
+public class Page1Mahasiswa extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page2_dosen);
-        RecyclerView recyclerView =findViewById(R.id.recyclerView);
+        setContentView(R.layout.activity_page1_mahasiswa);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) RecyclerView recyclerView =findViewById(R.id.recyclerView);
         List<MyData2> items2 = new ArrayList<MyData2>();
         items2.add(new MyData2("An-Naba", "20-10-2023", "Selesai"));
 
@@ -25,22 +27,12 @@ public class Page2Dosen extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new MyAdapter2(getApplicationContext(),items2));
 
-        View btninput = findViewById(R.id.btninput);
-        btninput.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Ketika tombolSignIn ditekan, pindah ke Page1Dosen
-                Intent intent = new Intent(Page2Dosen.this, Page3Dosen.class);
-                startActivity(intent);
-            }
-        });
-
-        View btnstatistik = findViewById(R.id.btnstatistik);
+        View btnstatistik = findViewById(R.id.btn1);
         btnstatistik.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(Page2Dosen.this, Page4Dosen.class);
+                Intent intent = new Intent(Page1Mahasiswa.this, Page2Mahasiswa.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/myapplication/Page2Mahasiswa.java
@@ -0,0 +1,87 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Page2Mahasiswa extends AppCompatActivity {
+
+    ProgressBar progressbar1;
+    ProgressBar progressbar2;
+    ProgressBar progressbar3;
+    ProgressBar progressbar4;
+
+    int counter1 = 0;
+    int counter2 = 0;
+    int counter3 = 0;
+    int counter4 = 0;
+
+    int maxCounter1 = 50; // Batas counter untuk ProgressBar 1
+    int maxCounter2 = 70; // Batas counter untuk ProgressBar 2
+    int maxCounter3 = 80; // Batas counter untuk ProgressBar 3
+    int maxCounter4 = 90; // Batas counter untuk ProgressBar 4
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2_mahasiswa);
+
+        progressbar1 = findViewById(R.id.progressbar1);
+        progressbar2 = findViewById(R.id.progressbar2);
+        progressbar3 = findViewById(R.id.progressbar3);
+        progressbar4 = findViewById(R.id.progressbar4);
+
+        // Memulai ProgressBar
+        startProgressBar(progressbar1, maxCounter1);
+        startProgressBar(progressbar2, maxCounter2);
+        startProgressBar(progressbar3, maxCounter3);
+        startProgressBar(progressbar4, maxCounter4);
+
+        View btnback = findViewById(R.id.arrowback);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Page2Mahasiswa.this, Page1Mahasiswa.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void startProgressBar(final ProgressBar progressBar, final int maxCounter) {
+        final Timer t = new Timer();
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Menambah nilai counter
+                        if (progressBar.getProgress() < maxCounter) {
+                            progressBar.setProgress(progressBar.getProgress() + 1);
+                        }
+
+                        // Jika counter mencapai batas, hentikan Timer
+                        if (progressBar.getProgress() == maxCounter)
+                            t.cancel();
+                    }
+                });
+            }
+        };
+
+        // Menentukan interval TimerTask berdasarkan nilai maksimum counter
+        // Semakin tinggi nilai maxCounter, semakin cepat intervalnya
+        long interval = 500 / maxCounter; // Misalnya, interval default 500ms dibagi dengan maxCounter
+
+        // Menjadwalkan TimerTask untuk dijalankan dengan interval yang telah dihitung
+        t.schedule(tt, 0, interval);
+    }
+}
